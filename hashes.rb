02cf23834929d1565585288373b9dae73c47911e
lmbeccaria <luis.m.beccaria@gmail.com>
2014-08-04 09:52:50 -0400

    Minor change in hashes.rb

--- hashes.rb
+++ hashes.rb
@@ -1,9 +1,13 @@
+require 'pry'
+
+# Returns a new hash with the fields that are the different between
+# the old and new hashes. It recursively verify nested hashes.
+
 def diffs_in_hash(old, new)
   diff = {}
-
   old.each do |key,old_value|
     if old_value.is_a?Hash
-      recurse = find_diffs(old_value, new[key])
+      recurse = diffs_in_hash(old_value, new[key])
       diff[key] = recurse if recurse != nil
     elsif new[key] != old_value
       diff[key] = [old_value, new[key]]
